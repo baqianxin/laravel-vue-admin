74e26e1d1c3eeecc7e8bc1769bf692354c4be6f7
baqianxin <baqianxin@163.com>
2021-02-02 15:27:10 +0800

    fix action button emit event

--- resources/js/components/widgets/Actions/ActionButton.vue
+++ resources/js/components/widgets/Actions/ActionButton.vue
@@ -105,15 +105,32 @@ export default {
     },
     onRequest(uri) {
       this.loading = true;
-      this.$http
-        .get(uri)
-        .then(res => {
-          if (res.code == 200) {
-          }
-        })
-        .finally(() => {
-          this.loading = false;
-        });
+      this.beforeEmit();
+      this.$http[this.action.requestMethod](uri)
+              .then(res => {
+                if (res.code == 200) {
+                  this.successEmit();
+                }
+              })
+              .finally(() => {
+                this.loading = false;
+                this.afterEmit();
+              });
+    },
+    beforeEmit() {
+      this.action.beforeEmit.map(item => {
+        this.$bus.emit(item.eventName, item.eventData);
+      });
+    },
+    afterEmit() {
+      this.action.afterEmit.map(item => {
+        this.$bus.emit(item.eventName, item.eventData);
+      });
+    },
+    successEmit() {
+      this.action.successEmit.map(item => {
+        this.$bus.emit(item.eventName, item.eventData);
+      });
     }
   },
   computed: {
